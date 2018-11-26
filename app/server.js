63b0eab3093af024495c5217fa6ae1d47d9cb884
Chris Del <drumgod101@gmail.com>
2018-11-26 18:02:51 -0500

    try to get request to pass back and forth. Client not working

--- app/server.js
+++ app/server.js
@@ -27,7 +27,7 @@ const client = new Twitter({
 // 		console.log('ERROR', error)
 // 	}
 // });
-var decode = ''
+var imageToClient = ''
 https.createServer(function(req, res) {
 
 	console.log(`${req.method} request for ${req.url}`)
@@ -49,85 +49,118 @@ https.createServer(function(req, res) {
 		});
 
 	} else if (req.url === "/twitter") {
-		// receieves a POST
-		let body = [];
-		new Promise((resolve, reject) => {
-				req.on('error', (err) => {
-					if (err) {
-						console.error(err);
-						reject(error)
-					}
-				}).on('data', (chunk) => {
-					// console.log(chunk)
-					body.push(chunk);
-				}).on('end', () => {
-					// if (body === typeof 'string') {
-					body = Buffer.concat(body).toString();
-					// }
-					// var request = require('request'),
-					// 	url = 'http://upload.wikimedia.org/wikipedia/commons/8/8c/JPEG_example_JPG_RIP_025.jpg';
-
-					// console.log()
-					// fs.writeFile('downloaded.jpg', body, 'binary', function(err) {});
-					// console.log(body)
-					// console.log('body', body.substring(1, 150))
-					// console.log('body', body)
-					resolve(body)
-				});
-
-				// decode = body
-				// console.log('push to decode')
+		if (req.method === 'GET') {
+			console.log('get req')
+			// console.log(typeof imageToClient)
+			console.log(imageToClient)
+			let params = {
+				command: 'STATUS',
+				media_id: imageToClient.media_id_string
+			}
+			console.log('params', params)
+			client.get('media/upload', params, function(error, upload, response) {
+				console.log('get made to status')
+				console.log('first', upload);
 			})
-			// })
-			.then(base64Data => {
-				// console.log('below', base64Data.substring(1, 150))
-				base64Data = decodeBase64Image(base64Data)
-				let file = fs.writeFileSync('clock.png', base64Data.data, 'base64')
+			let params1 = {
+				command: 'STATUS',
+				media_id: imageToClient.media_id
+			}
+			client.get('media/upload', params1, function(error, upload, response) {
+				console.log('get made to status')
+				console.log('second', upload);
+			})
 
-
-				var params = {
-					screen_name: 'nodejs'
-				};
-				client.get('statuses/user_timeline', params, function(error, tweets, response) {
-					if (!error) {
-						console.log(tweets);
-					}
-				});
-				let imageFile = fs.readFileSync('/Users/chrisdielschnieder/desktop/code_work/vue/clock_html/app/clock.png')
-				// console.log('image', image)
-				client.post('media/upload', {
-					media: imageFile
-				}, function(error, media, response) {
-
-					if (!error) {
-
-						// If successful, a media object will be returned.
-						console.log(media);
-
-						// Lets tweet it
-						var status = {
-							status: 'I am a tweet',
-							media_ids: media.media_id_string // Pass the media id string
+			// var params = {
+			// 	screen_name: 'The_Seperator'
+			// };
+			// client.get('statuses/user_timeline', params, function(error, tweets, response) {
+			// 	if (!error) {
+			// 		console.log(tweets);
+			// 	} else {
+			// 		console.error('error:', error)
+			// 	}
+			// });
+			res.end()
+		} else if (req.method === 'POST') {
+			console.log('post')
+			// receieves a POST
+			let body = [];
+			new Promise((resolve, reject) => {
+					req.on('error', (err) => {
+						if (err) {
+							console.error(err);
+							reject(error)
 						}
+					}).on('data', (chunk) => {
+						// console.log(chunk)
+						body.push(chunk);
+					}).on('end', () => {
+						// if (body === typeof 'string') {
+						body = Buffer.concat(body).toString();
+						// }
+						// var request = require('request'),
+						// 	url = 'http://upload.wikimedia.org/wikipedia/commons/8/8c/JPEG_example_JPG_RIP_025.jpg';
 
-						client.post('statuses/update', status, function(error, tweet, response) {
-							if (!error) {
-								console.log(tweet);
-							}
-						});
+						// console.log()
+						// fs.writeFile('downloaded.jpg', body, 'binary', function(err) {});
+						// console.log(body)
+						// console.log('body', body.substring(1, 150))
+						// console.log('body', body)
+						resolve(body)
+					});
 
-					}
-				});
-				res.end()
-			}).catch((e) => {
+					// decode = body
+					// console.log('push to decode')
+				})
+				// })
+				.then(base64Data => {
+					// console.log('below', base64Data.substring(1, 150))
+					base64Data = decodeBase64Image(base64Data)
+					let file = fs.writeFileSync('clock.png', base64Data.data, 'base64')
 
-				console.error('error', e)
-			})
 
-		req.on('error', (e) => {
-			console.error(`problem with request: ${e.message}`);
-		});
-		// 	// req.write(image)
+					// var params = {
+					// 	screen_name: 'The_Seperator'
+					// };
+					// client.get('statuses/user_timeline', params, function(error, tweets, response) {
+					// 	if (!error) {
+					// 		console.log(tweets);
+					// 	}
+					// });
+					let imageFile = fs.readFileSync('/Users/chrisdielschnieder/desktop/code_work/vue/clock_html/app/clock.png')
+					// console.log('image', image)
+					client.post('media/upload', {
+						media: imageFile
+					}, function(error, media, response) {
+
+						if (!error) {
+							// If successful, a media object will be returned.
+							imageToClient = media
+							console.log('uploaded')
+							// var status = {
+							// 	status: 'I am a tweet',
+							// 	media_ids: media.media_id_string // Pass the media id string
+							// }
+							//
+							// client.post('statuses/update', status, function(error, tweet, response) {
+							// 	if (!error) {
+							// 		console.log(tweet);
+							// 	}
+							// });
+
+						}
+						console.log('Done')
+						res.end()
+					})
+				}).catch((e) => {
+
+					console.error('error', e)
+				})
+			req.on('error', (e) => {
+				console.error(`problem with request: ${e.message}`);
+			});
+		} // 	// req.write(image)
 	} else if (req.url === '/hello') {
 
 		// var request = require('request'),
